245df3f294c27bccd09b12938953dbdc7fdfcce9
lamThanakit <h58284836@gmail.com>
2024-12-06 12:40:44 +0700

    first commit

---
+++ src/routes/customers.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express.Router();
+const controller = require('../controller/customer.controller');
+
+app.get("/", controller.get);
+
+app.get("/:id", controller.getById);
+
+app.get("/name/:name", controller.getname);
+
+app.post("/", controller.create);
+
+app.put("/:id", controller.editall);
+
+app.patch("/:id", controller.edit);
+
+app.delete("/:id", controller.de);
+
+module.exports = app;
